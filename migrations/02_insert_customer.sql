b39e4e4a048b2b0b581555e53aed40dd78fdf8ea
SlaktarMicke <shroggy.onthehuus@gmail.com>
2025-11-22 15:56:40 +0100

    feat: add insert into example

---
+++ migrations/02_insert_customer.sql
@@ -0,0 +1,11 @@
+INSERT INTO customer (
+    customer_id,
+    first_name,
+    surname,
+    ssn
+) VALUES (
+    '019aac0b-38d8-7b61-8b51-f0a35cc8db6e',
+    'Olle',
+    'Bolle',
+    '180012120101'
+)
